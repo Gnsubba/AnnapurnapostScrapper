0a4b4d49aa1e944269d2a88e1fc70e02b2245e8f
Gn subba <gnsubba@gmail.com>
2022-06-18 13:19:18 +0545

    Added pagination feature

--- web2.py
+++ web2.py
@@ -12,9 +12,15 @@ search_keyword = 'राजनीति'
 
 url = 'https://bg.annapurnapost.com/api/search?title='+search_keyword
 
+pagination = []
+
+for i in range(3):
+    pagination.append(False)
 
 # print(url)
 for i in range(3):
+    if pagination == True:
+        continue
     if i == 0:
         try:
             r = requests.get(url)
@@ -35,5 +41,6 @@ for i in range(3):
     news = {"page"+str(i+1)+"news" + str(j+1): items[j] for j in range(len(items))}
 
     print(news)
+    pagination[i] = True
 
     # print(news_json)
